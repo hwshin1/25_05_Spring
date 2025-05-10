5e0fcad686f20858e27f8d9dc23a9f2f476a94a3
hwshin1 <hwshin5044@gmail.com>
2025-05-10 21:25:25 +0900

    Test 데이터 생성

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -17,6 +17,17 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		lastArticleId = 0;
 		articles = new ArrayList<>();
+		
+		makeTestData();
+	}
+	
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+			
+			doWrite(title, body);
+		}
 	}
 	
 	@RequestMapping("/usr/article/doWrite")
